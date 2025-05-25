b9353b102865e703aaed689283057f3d791a5453
xiaolu2meixue <2369200071@qq.com>
2025-05-25 09:52:59 -0700

    add note

---
+++ C-launage/day1/02hello.c
@@ -0,0 +1,12 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+
+    printf("hello world\n");
+    //功能：向终端中输出字符串数据
+    //
+    return 0;
+}
