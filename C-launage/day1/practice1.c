ecf4e4953af342cdc2a34898bf159b0e3a3a7d08
xiaolu2meixue <2369200071@qq.com>
2025-05-25 23:07:29 -0700

    change

--- C-launage/day1/practice1.c
+++ C-launage/day1/practice1.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 int main(int argc, const char *argv[])
 {
-	printf("静夜思\n唐 李白\n床前明月光，疑是地上霜。\n举头望明月，低头思故乡。\n");
+	printf("\t\t\t静夜思\n\t\t\t唐 李白\n\t\t床前明月光，疑是地上霜。\n\t\t举头望明月，低头思故乡。\n");
 	return 0;
 }
 
