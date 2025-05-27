cf73bdd211ee52f9f9b55c6932b363a61c5d287a
xiaolu2meixue <2369200071@qq.com>
2025-05-26 21:02:25 -0700

    change

---
+++ C-launage/day1/practice2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, const char *argv[])
+{
+	printf("%d\n",'\011');    //9
+	printf("%d\n",'\x11');    //17
+
+	return 0;
+}
