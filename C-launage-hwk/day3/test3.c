5052f819b5a602af7f389d3276aaed7a3e542bb2
xiaolu2meixue <2369200071@qq.com>
2025-05-28 18:19:47 -0700

    change

--- C-launage-hwk/day3/test3.c
+++ C-launage-hwk/day3/test3.c
@@ -5,13 +5,9 @@ int main(int argc, const char *argv[])
 {
 	int n;
 	scanf("%d",&n);
-	if(0==n%3&0==n%5&&0==n%7) printf("3 5 7\n");
-	else if(0==n%3&&0==n%5) printf("3 5\n");
-	else if(0==n%5&&0==n%7) printf("5 7\n");
-	else if(0==n%3&&0==n%7) printf("3 7\n");
-	else if(0==n%3) printf("3\n");
-	else if(0==n%5) printf("5\n");
-	else if(0==n%7) printf("7\n");
+	if(0==n%3) printf("3 ");
+	if(0==n%5) printf("5 ");
+	if(0==n%7) printf("7\n");
 	else printf("%d\n",n);
 	return 0;
 }
