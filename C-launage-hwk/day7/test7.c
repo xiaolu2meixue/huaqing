8b9e38c09c74a9a9220e2a2676009d3b1fe396c8
xiaolu2meixue <2369200071@qq.com>
2025-06-04 08:38:55 -0700

    add

--- C-launage-hwk/day7/test7.c
+++ C-launage-hwk/day7/test7.c
@@ -1,29 +1,30 @@
-#include <myhead.h>
-int main(int argc, const char *argv[])
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
 {
-    int i;
-    char str[][10] = {};
-	for ( i = 0; i < 5; i++)
-    {
+    //int i;
+    char str[5][10] = { 0 };
+    char temp[10] = { 0 };
+    for (int i = 0; i < 5; i++) {
         printf("请输入姓名：");
-        puts(str[i]);
+        fgets(str[i], 10, stdin);
     }
-    for (int j = 0; j < 5; j++)
-    {
-        for (int q = j; q < j + 5 - 1; q++)
-        {
-            if (strcmp(str[q], str[q + 1]) > 0)
-            {
-                char temp;
-                strcpy(temp, str[q]);
-                strcpy(str[q], str[q + 1]);
-                strcpy(str[q + 1], temp);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4 - i; j++) {
+            if (strcmp(str[j], str[j + 1]) > 0) {
+                
+                strcpy(temp, str[j]);
+                strcpy(str[j], str[j + 1]); 
+                strcpy(str[j + 1], temp);
             }
-            
+            memset(temp, 0, sizeof(temp));
+
         }
-        
+
     }
-    
-    
-	return 0;
+
+    for (int i = 0; i < 5; i++) fputs(str[i], stdout);
+    return 0;
 }
