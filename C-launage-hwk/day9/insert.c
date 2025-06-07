9c7d764de816a2f9baae61bf505f50e0c29bdc12
xiaolu2meixue <2369200071@qq.com>
2025-06-06 21:02:02 -0700

    change

--- C-launage-hwk/day9/insert.c
+++ C-launage-hwk/day9/insert.c
@@ -4,8 +4,7 @@ int Arr(int* arr)
 {
 	int num;
     //循环输入数组
-	for (int count = 0; ;count++)
-	{
+	for (int count = 0; ;count++) {
 		printf("请输入：（输入-1结束输入）");
 		scanf("%d", &num);
 		if (num == -1) return count; //输入-1结束输入
@@ -15,11 +14,11 @@ int Arr(int* arr)
 }
 void insert(int* arr1, int len1, int which1, int num1)
 {
-    int* insert1 = &arr1[which1 - 1];
-    arr1[len1] = num1;
+    int* insert1 = arr1 + which1 - 1;
+    arr1[len1] = num1; //先把插入的数字插入到数组末尾
     int* end = arr1 + len1;
-    for ( ; insert1 < end; insert1++)
-    {
+    //调换顺序，重组数组
+    for ( ; insert1 < end; insert1++) {
         int temp = *insert1;
         *insert1 = *end;
         *end = temp;
@@ -30,22 +29,22 @@ int main(int argc, const char *argv[])
 	int array[100];
 	int len = Arr(array); //传递数组与数组长度
     int which;
+    // 输入插入位置，并确保插入位置不大于数组长度
     for ( ; ; ) {
         printf("请输入在数组第几个元素插入：");
         scanf("%d", &which);
         if (which <= len + 1) {
             break;
         }
-        else printf("输入错误，重新输入"); // 确保插入位置小于数组长度
+        else printf("输入错误，重新输入");
     }
-    printf("请输入需要插入的元素：");
+    printf("请输入需要插入的元素："); //输入插入的元素
     int num;
     scanf("%d", &num);
     insert(array, len, which, num); //调用插入函数
 
     //循环打印数组
-    for (int i = 0; i <= len; i++)
-    {
+    for (int i = 0; i <= len; i++) {
         printf(" %d",array[i]);
     }
     printf("\n");
