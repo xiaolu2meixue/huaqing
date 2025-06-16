6e1601d50a36fd22ea3a9f5cf634794753f02cdc
xiaolu2meixue <2369200071@qq.com>
2025-06-16 06:33:27 -0700

    [Huaqing][E6766][TP][Bugfix]:数据结构day2 describe:上传day2数据结构源码
    bugid:NA
    branch path:
    
    Signed-off-by: xiaolu2meixue <2369200071@qq.com>

---
+++ data_struct_hwk/day2/link.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <myhead.h>
+
+typedef struct node {
+    union {
+        int data;
+        int len;
+    };
+    struct node* next;
+}node;
+
+node* creat_link();
+node* creat_node(int value);
+int empty_link(node* H);
+void insert_head(node* H, int value);
+void insert_tail(node* H, int value);
+void show_link(node* H);
+void delete_head(node* H);
+void delete_tail(node* H);
+void delete_tail(node* H);
+void insert_pos(node* H);
+void delete_pos(node* H);
+void find_pos(node* H);
+void change_ele(node* H);
+void invert_link(node* H);
+void invert_link2(node* H);
